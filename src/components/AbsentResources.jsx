7357dba9deb6ae26ceebb4c87d969d2cf773c7be
Muhammad Talha <m.talha@hazentech.com>
2024-10-18 22:12:11 +0500

    minor chganges

--- src/components/AbsentResources.jsx
+++ src/components/AbsentResources.jsx
@@ -2,11 +2,12 @@ import BreadCrumb from "./BreadCrumb"
 import SelectionTable from "./SelectionTable"
 
 import { GrSearch } from "react-icons/gr"
-import { FaUserCheck } from "react-icons/fa"
+import { FaSave, FaUserCheck } from "react-icons/fa"
 import { HiTrash } from "react-icons/hi"
 
 import { InputSwitch } from "primereact/inputswitch"
 import { useState } from "react"
+import { RxCross2 } from "react-icons/rx"
 
 const users = [
     {id: 1, name: "Aliza Sehar", email: "aliza@gmail.com"},
@@ -15,7 +16,20 @@ const users = [
 ]
 
 const AbsentResources = () => {
-    const [checked, setChecked] = useState(false);
+    
+    const initialCheckedState = users.reduce((acc, user) => {
+        acc[user.id] = false; // or true if you want some to be checked by default
+        return acc;
+    }, {})
+
+  const [checkedStates, setCheckedStates] = useState(initialCheckedState);
+
+  const handleToggle = (userId) => {
+    setCheckedStates((prev) => ({
+      ...prev,
+      [userId]: !prev[userId], // Toggle the specific userId
+    }));
+  }
 
   return (
     <section className='p-5 h-full w-full'>
@@ -46,27 +60,41 @@ const AbsentResources = () => {
                     Absent/Partial Resources <span>(3)</span> 
                 </span>
             </h2>
-            <section className="w-full flex gap-3 flex-col">
-                {users?.map((user) => (
-                    <section className="p-3 w-full flex items-center justify-between border-2 border-[#00457C] rounded-md">
-                        <div className="flex gap-2 items-center">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR34VNI2RCmMP7q-xSlNft7ya1cNF_HxOZ-xA&s" alt="user image" className="w-10 h-10 object-cover rounded-full" />
-                            <span className="flex flex-col justify-between h-full font-Inter-Regular text-sm text-primary">
-                                <h3> {user.name} </h3> 
-                                <h3> {user.email} </h3> 
-                            </span>
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <h3 className="font-Inter-Regular text-sm text-black">Partially Available</h3>
-                            <div className="card flex justify-content-center">
-                                <InputSwitch checked={checked} onChange={(e) => setChecked(e.value)} />
+            <main className="w-full h-full flex flex-col justify-between">
+                <section className="w-full flex gap-3 flex-col">
+                    {users?.map((user) => (
+                        <section key={user.id} className="p-3 w-full flex items-center justify-between border-2 border-[#00457C] rounded-md">
+                            <div className="flex gap-2 items-center">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR34VNI2RCmMP7q-xSlNft7ya1cNF_HxOZ-xA&s" alt="user image" className="w-10 h-10 object-cover rounded-full" />
+                                <span className="flex flex-col justify-between h-full font-Inter-Regular text-sm text-primary">
+                                    <h3> {user.name} </h3> 
+                                    <h3> {user.email} </h3> 
+                                </span>
                             </div>
-                            <HiTrash size={25} color="#CB4B6C" />
-                        </div>
-                    </section>
-                ))}
-
-            </section>
+                            <div className="flex items-center gap-2">
+                                <h3 className="font-Inter-Regular text-sm text-black">Partially Available</h3>
+                                <div className="card flex justify-content-center">
+                                    <InputSwitch 
+                                        checked={checkedStates[user.id]} 
+                                        onChange={() => handleToggle(user.id)} 
+                                    />
+                                </div>
+                                <HiTrash size={25} color="#CB4B6C" />
+                            </div>
+                        </section>
+                    ))}
+                </section>
+                <div className="mt-5 flex items-center gap-3">
+                    <button type="button" className="font-Inter-Regular text-sm rounded-md bg-[#00457C] flex items-center gap-2 py-2 px-3 text-white">
+                        <FaSave />
+                        Save
+                    </button>
+                    <button type="button" className="font-Inter-Regular text-sm rounded-md text-[#00457C] border border-[#00457C] flex items-center gap-1 py-2 px-3 bg-transparent">
+                        <RxCross2 />
+                        Cancel
+                    </button>
+                </div>
+            </main>
         </section>
       </main>  
     </section>
